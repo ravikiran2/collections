c7956ab4deaaf4cce9d5abe6df0f8fc46f9946eb
ravikiran2 <47805907+ravikiran2@users.noreply.github.com>
2019-04-05 19:48:56 +0530

    Iterator - List interface with ArrayList class

---
+++ IteratorExampleTest.java
@@ -0,0 +1,12 @@
+package com.java.collections;
+
+public class IteratorExampleTest {
+
+	public static void main(String[] args) {
+		IteratorExample i=new IteratorExample();
+		i.info();
+		
+		
+	}
+
+}
